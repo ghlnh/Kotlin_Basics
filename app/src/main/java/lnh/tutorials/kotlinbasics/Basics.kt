855dad2182a34bb63c4df56480c8bfa156de8c83
ghlnh <110441894+ghlnh@users.noreply.github.com>
2023-03-30 15:08:21 +0900

    Initial commit

---
+++ app/src/main/java/lnh/tutorials/kotlinbasics/Basics.kt
@@ -0,0 +1,15 @@
+package lnh.tutorials.kotlinbasics
+
+//fun은 함수를 의미
+fun main() {    //main이 앱의 시작점이여서 플레이버튼이 main에만 있음
+    var myName = "LNH"
+    myName = "lnh"
+
+    val name = "LNH"
+    //name = "lnh"
+    print("Hello" + myName)
+    print("Hello"+ name)
+}
+fun hello(){
+
+}
