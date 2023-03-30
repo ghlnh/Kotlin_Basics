357cc68486fde854a726c5331eaede44b6b3adeb
ghlnh <110441894+ghlnh@users.noreply.github.com>
2023-03-30 15:24:39 +0900

    숫자 데이터 타입(자바와 동일)

--- app/src/main/java/lnh/tutorials/kotlinbasics/Basics.kt
+++ app/src/main/java/lnh/tutorials/kotlinbasics/Basics.kt
@@ -2,14 +2,17 @@ package lnh.tutorials.kotlinbasics
 
 //fun은 함수를 의미
 fun main() {    //main이 앱의 시작점이여서 플레이버튼이 main에만 있음
-    var myName = "LNH"
-    myName = "lnh"
-
     val name = "LNH"
-    //name = "lnh"
-    print("Hello" + myName)
-    print("Hello"+ name)
-}
-fun hello(){
+    //type int 32bit
+    var myAge = 31
+    val myByte: Byte = 13
+    val myShort: Short = 125
+    val myInt: Int = 123123123
+    val myLong: Long = 12_039_812_309_487_120_3
+
+    //Floating Point number Type:Float (32bit), Double(64bit)
+    val myFloat: Float = 13.37F
+    val myDouble: Double = 3.145924565165486334545231321354654513213546462
+    print("Hello" + name)
 
 }
