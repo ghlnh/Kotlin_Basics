69a0115e3a35ce74d37d4b0de2bf841038dbb9b1
ghlnh <110441894+ghlnh@users.noreply.github.com>
2023-04-11 16:35:00 +0900

    클래스와 객체 연습문제

--- app/src/main/java/lnh/tutorials/kotlinbasics/KotlinBasics.kt
+++ app/src/main/java/lnh/tutorials/kotlinbasics/KotlinBasics.kt
@@ -5,6 +5,11 @@ fun main() {
     var denis = Person("Denis", "Panjuta")
     var john = Person()
     var johnPeterson = Person(lastName = "Peterson")
+
+    //연습 문제
+    var SamsungGalaxyS20Ultra = MobliePhone(osName = "Android", brand = "Samsung", model = "Galaxy S20 Ultra")
+    var AppleIphoneSe = MobliePhone(osName = "Ios", brand = "Apple", model = "IphoneSe")
+    var AppleIphone13 = MobliePhone()
     }
 //constructor()는 보조 생성자로 객체 생성 시 값을 추가함
     class Person constructor(firstName: String = "John", lastName: String = "Doe"){ //디폴트 값 John Doe 지정 가능
@@ -12,5 +17,13 @@ fun main() {
         init {
             println("Initialized a new Person object with "+
                     "firstName = $firstName and lastName = $lastName")
+        }
+    }
+
+    //연습 문제
+    class MobliePhone constructor(osName : String ="Ios", brand: String ="Apple", model: String = "Iphone13"){
+        init {
+            println("Initialized a new MobliePhone object with "+
+                    "osName = $osName and brand = $brand and model = $model")
         }
     }
