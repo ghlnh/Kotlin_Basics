273ef296ff5795d03aae0e127a9ee30d0750a28c
ghlnh <110441894+ghlnh@users.noreply.github.com>
2023-04-25 18:07:50 +0900

    배열

--- app/src/main/java/lnh/tutorials/kotlinbasics/Basics.kt
+++ app/src/main/java/lnh/tutorials/kotlinbasics/Basics.kt
@@ -44,7 +44,52 @@ fun avg(a: Double, b: Double) : Double {
     println(nullableName!!.toLowerCase())
 
 
+    //배열
+    //val numbers:IntArray = intArrayOf(1, 2, 3, 4, 5, 6)
+
+    //아래와 같이 타입 추론을 이용해 더 간단하게 배열 생성
+    val numbers = intArrayOf(1, 2, 3, 4, 5, 6)
+    val numbersD:DoubleArray = doubleArrayOf(1.0,2.0,3.0,4.0,5.0,6.0)
+    println(numbers)  //numbers가 저장된 주소가 출력됨
+    println(numbers.contentToString())  //numbers 배열이 출력됨 [1, 2, 3, 4, 5, 6]
+
+    //for문 이용해 numbers 요소+2 값을 하나씩 출력
+    for(i in numbers){
+        print("${i+2}")
+    }
+
+    //배열 수정
+    numbers[0] = 6
+    numbers[1] = 5
+    numbers[4] = 2
+    numbers[5] = 1
+    print("\nfinal values ${numbers.contentToString()}")
+
+    numbersD[0] = 6.0
+    numbersD[1] = 5.0
+    numbersD[4] = 2.0
+    numbersD[5] = 1.0
+    print("\nfinal values ${numbersD.contentToString()}")
+
+    val days = arrayOf("Sun","Mon","Tues","Wed","Thurs","Fri","Sat")
+    println(days.contentToString())
+
+    val fruits = arrayOf(Fruit("Apple", 2.5), Fruit("Grape", 3.5))
+    println(fruits.contentToString())
+    //fruits의 이름만 뽑아서 프린트
+    for (fruit in fruits){
+        println("${fruit.name}")
+    }
+
+    for(index in fruits.indices){  //indices는 인덱스를 알려줌
+        println("${fruits[index].name} is in index $index")
+    }
+
+    //다양한 데이터 타입이 있는 배열
+    val mix = arrayOf("Sun","Mon","Tues",1,2,3,Fruit("Apple",2.5))
+    println(mix.contentToString())
 }
+data class Fruit(val name:String, val price:Double)
 
 
 fun myFunction(){
